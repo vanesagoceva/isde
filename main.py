2455a677e83760567ead52f37a5d601e2ad2dd3f
vanesagoceva <vanesagoceva@gmail.com>
2025-05-27 15:34:20 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+import json
+from typing import Dict, List
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from app.config import Configuration
+from app.forms.classification_form import ClassificationForm
+from app.ml.classification_utils import classify_image
+from app.utils import list_images
+
+app = FastAPI()
+config = Configuration()
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/info")
+def info() -> Dict[str, List[str]]:
+    """Returns available models and images."""
+    return {
+        "models": Configuration.models,
+        "images": list_images()
+    }
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@app.get("/classifications")
+def create_classify(request: Request):
+    return templates.TemplateResponse(
+        "classification_select.html",
+        {"request": request, "images": list_images(), "models": Configuration.models}
+    )
+
+@app.post("/classifications")
+async def request_classification(request: Request):
+    form = ClassificationForm(request)
+    await form.load_data()
+    image_id = form.image_id
+    model_id = form.model_id
+    scores = classify_image(model_id=model_id, img_id=image_id)
+    return templates.TemplateResponse(
+        "classification_output.html",
+        {
+            "request": request,
+            "image_id": image_id,
+            "classification_scores": json.dumps(scores)
+        }
+    )
